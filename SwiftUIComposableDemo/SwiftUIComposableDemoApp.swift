6d36801265cd4c6efd85cf96f039b5552a93eb05
Tomáš Horáček <tomas.horacek@gmail.com>
2021-07-20 16:50:11 +0200

    Initial Commit

---
+++ SwiftUIComposableDemo/SwiftUIComposableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComposableDemoApp.swift
+//  SwiftUIComposableDemo
+//
+//  Created by Tomáš Horáček on 20.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComposableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
